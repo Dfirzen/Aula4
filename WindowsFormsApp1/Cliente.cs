101cb32db0b4566765c2830c710d3349073bdb32
Diego-notebook <Diego@DESKTOP-RN26VKI>
2023-08-24 21:10:25 -0300

    inserindo o = "" para resolver os problemas de campos vazios

--- WindowsFormsApp1/Cliente.cs
+++ WindowsFormsApp1/Cliente.cs
@@ -8,12 +8,12 @@ namespace WindowsFormsApp1
 {
     internal class Cliente
     {
-        private string nome;
-        private string endereco;
-        private string email;
-        private string estado;
-        private string login;
-        private string senha;
+        private string nome = "";
+        private string endereco = "";
+        private string email = "";
+        private string estado = "";
+        private string login = "";
+        private string senha = "";
 
         public string Nome { get => nome; set => nome = value; }
         public string Endereco { get => endereco; set => endereco = value; }
